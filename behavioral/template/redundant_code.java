ce1fbaba7a7ee57596211f9f0d2977d06e35e5d0
Sean <psckyky@hotmail.com>
2022-12-15 14:12:52 +0900

    Create redundant_code.java

---
+++ behavioral/template/redundant_code.java
@@ -0,0 +1,49 @@
+public class IncheonAirport {
+    public void check() {
+        checkPassport();
+        checkBaggage();
+        checkTicket();
+        checkFlight();
+        checkAirportLocation();
+    }
+    public void checkPassport() {
+        System.out.println("check passport");
+    }
+    public void checkBaggage() {
+        System.out.println("check baggage");
+    }
+    public void checkTicket() {
+        System.out.println("check ticket");
+    }
+    public void checkFlight() {
+        System.out.println("check flight");
+    }
+    public void checkAirportLocation() {
+        System.out.println("check airport location");
+    }
+}
+
+public class GimpoAirport {
+    public void check() {
+        checkPassport();
+        checkBaggage();
+        checkTicket();
+        checkFlight();
+        checkAirportLocation();
+    }
+    public void checkPassport() {
+        System.out.println("check passport");
+    }
+    public void checkBaggage() {
+        System.out.println("check baggage");
+    }
+    public void checkTicket() {
+        System.out.println("check ticket");
+    }
+    public void checkFlight() {
+        System.out.println("check flight");
+    }
+    public void checkAirportLocation() {
+        System.out.println("check airport location");
+    }
+}
